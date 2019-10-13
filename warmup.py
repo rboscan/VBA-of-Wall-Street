6ccb267f632f213760319682f13f9b77013514c8
rboscan <51136553+rboscan@users.noreply.github.com>
2019-10-13 16:57:04 -0400

    Final Commit

---
+++ warmup.py
@@ -0,0 +1,12 @@
+print("Hello user!")
+
+name1 = input("What is your name?")
+
+print("Hello " + name1)
+
+age = input(int("What is your age, " +str(name) + "?" ))
+
+    if int(age) < 50 then
+    print("Aww you're just a baby!")
+    else: print("Ah... A well traveled soul you are!")
+
